c09abc50063f18301b9304c01b2139949c326056
Theplayer592 <lukaanddylan@gmail.com>
2023-02-20 20:36:54 +0000

    Updated setup experience to provide user feedback in case npm or node is not installed

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
+if ! npm -v &> /dev/null
+  then
+    echo "npm must be installed for program to run"
+    exit 1
+fi
+
+if ! node -v &> /dev/null
+  then 
+    echo "node.js must be installed for program to run"
+    exit 1
+fi
+
 npm install -g npm
 npm install
 npm install typescript --save-dev
